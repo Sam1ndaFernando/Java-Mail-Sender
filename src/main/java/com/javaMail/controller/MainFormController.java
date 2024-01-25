c6f5609c833b28b4ac2cbb257d7cc7ab8becc2f0
Sam1ndaFernando <uthpala2001@gmail.com>
2024-01-25 12:00:36 +0530

    btnAttachmentOnAction code complete

--- src/main/java/com/javaMail/controller/MainFormController.java
+++ src/main/java/com/javaMail/controller/MainFormController.java
@@ -8,7 +8,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -43,8 +46,41 @@ public class MainFormController implements Initializable {
 
     @FXML
     void btnAttachmentOnAction(ActionEvent event) {
+        lblAttachedFileLabel.setVisible(true);
+        txtAttachedFile.setVisible(true);
 
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose File to Attach");
+
+        // Uncomment the following lines if you want to add filters for PDF and all files
+    /*
+    fileChooser.getExtensionFilters().addAll(
+            new ExtensionFilter("PDF Files", "*.pdf"),
+            new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif"),
+            new ExtensionFilter("All Files", "*.*")
+    );
+    */
+
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        if (selectedFile != null) {
+            String fileName = selectedFile.getName();
+            String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+            if ("png".equalsIgnoreCase(fileExtension) || "jpg".equalsIgnoreCase(fileExtension) || "jpeg".equalsIgnoreCase(fileExtension) || "gif".equalsIgnoreCase(fileExtension)) {
+                txtAttachedFile.setText(selectedFile.getName());
+            } else {
+                System.out.println("Selected file is not an image.");
+                // Optionally, you may want to clear the text field if the selected file is not an image
+                txtAttachedFile.clear();
+            }
+        } else {
+            System.out.println("File selection canceled.");
+            // Optionally, you may want to clear the text field if the user cancels the file selection
+            txtAttachedFile.clear();
+        }
     }
+
 
     @FXML
     void btnSendOnAction(ActionEvent event) {
